853d3f0ca24688d7ac11d45f361eb8e6af887d98
zeasnelson <zeasnelson@gmail.com>
2020-02-28 19:19:16 -0500

    update

--- src/Tracker.java
+++ src/Tracker.java
@@ -1,7 +1,15 @@
 public class Tracker {
 
+    private Long swaps;
+    private Long comparisons;
+
+    private String algorithmName;
+
+    
+
+
 
     public static void main(String[] args) {
-        System.out.println("hello world");
+
     }
 }
