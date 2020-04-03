f91426e095c318c97a5e1ad84de44d5066060aa4
Alex Salazar <alxanderapollo@gmail.com>
2020-04-03 19:39:21 -0400

    alex is growing a fourth head

--- src/HashTableWithSeparateChaining.java
+++ src/HashTableWithSeparateChaining.java
@@ -6,26 +6,26 @@ public class HashTableWithSeparateChaining<K, V> implements Iterable<K> {
 	
 	
 	public class Entry<K, V> {
-		
+
 		  int hash; //hash
 		  K key;	//keys
 		  V value;  //values
-	
+
 		  public Entry(K key, V value) {
 		    this.key = key;
 		    this.value = value;
 		    this.hash = key.hashCode();
 		  }
-	
+
 		  // We are not overriding the Object equals method
 		  // No casting is required with this method.
-		  
-		  //equals method not being overidden 
+
+		  //equals method not being overidden
 		  public boolean equals(Entry<K, V> other) {
 		    if (hash != other.hash) return false;
 		    return key.equals(other.key);
 		  }
-		  
+
 		  //print out the the link of key 
 		  @Override
 		  public String toString() {
@@ -413,7 +413,7 @@ public class HashTableWithSeparateChaining<K, V> implements Iterable<K> {
 		sb.append("}");
 		return sb.toString();
 	}// end to string
-	
+
 	public static void main(String [] alex) {
 
 		//Random Numbers and sequential values
